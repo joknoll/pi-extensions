8d4b35a2503045225947436033b0e8d59fd1a4de
Johannes Knoll <git@joknoll.de>
2026-07-31 19:51:21 +0200

    Simplify setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite-plus";
 
 export default defineConfig({
+  pack: {
+    exports: true,
+    format: "esm",
+  },
   staged: {
     "*": "vp check --fix",
   },
-  fmt: {},
+  fmt: {
+    ignorePatterns: ["packages/*/dist/**"],
+  },
   lint: {
     jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
     rules: { "vite-plus/prefer-vite-plus-imports": "error" },
